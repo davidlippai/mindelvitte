bce1b52b5db82f94ff11d12dc0f58619df48d987
davidlippai <lippai28@gmail.com>
2021-12-30 19:37:59 +0100

    gallery html

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+/*===========HAM-MENU============*/
+
 $("#ham-menu").click(function(){
-  $("#menu").slideToggle("600");
+  $("#menu").slideToggle("600")
+  let firstLine = document.getElementById("line-one");
+  let secondLine = document.getElementById("line-two");
+  let thirdLine = document.getElementById("line-three");
+
+  firstLine.classList.toggle("line-one-ani");
+  secondLine.classList.toggle("line-two-ani");
+  thirdLine.classList.toggle("line-three-ani");
 });
+
+
+
+/*==========GALERY-VISIBLE======*/
+
+/*$("#gallery-link").click(function(){
+  $("#images").slideToggle("600");
+
+  let images = document.getElementById("images");
+  images.classList.toggle("visible-images")
+})*/
